fa5aa2aa51e0cbc4029e9e3ddcb37e1353eb982f
龙威 <longwei@youmi.net>
2013-11-25 11:50:39 +0800

    add ObservableArrayPrototype for extra detecting

--- Array.observe.js
+++ Array.observe.js
@@ -1,16 +1,11 @@
 ;(function(){
-//var startTime   = new Date().getTime();
-    var i,
-        _array              = [],
-        _sign               = [],
+    var _array              = [],
         ArrayPrototype      = Array.prototype,
         MAX                 = 9999,//Number.MAX_VALUE,//observe array's max length, if it's too big, browser will work slow
-        proto               = ObservableArray.prototype,
-        defineProperty      = Object.defineProperty,
         noticeNew           = function(i, newValue){
             //console.log('new',arguments,this)
             this.notification.call(this,{
-                "name"      : i,
+                "name"      : Number(i),
                 "object"    : this,
                 "type"      : "new",
                 "value"     : newValue
@@ -19,7 +14,7 @@
         noticeDelete        = function(i, oldValue){
             //console.log('delete',arguments,this)
             this.notification.call(this,{
-                "name"      : i,
+                "name"      : Number(i),
                 "object"    : this,
                 "type"      : "deleted",
                 "oldValue"  : oldValue
@@ -28,7 +23,7 @@
         noticeUpdated       = function(i, oldValue, newValue){
             //console.log('updated',arguments,this)
             this.notification.call(this,{
-                "name"      : i,
+                "name"      : Number(i),
                 "object"    : this,
                 "type"      : "updated",
                 "oldValue"  : oldValue,
@@ -53,6 +48,7 @@
             }catch(e){}
         },
         newItem             = function(i,value){
+            _array[i] = value;
             genGetterSetter.call(this,i);
             noticeNew.call(this,i,value);
         },
@@ -61,212 +57,130 @@
                 index;
             for(index in _array){
                 console.log(index)
-                noticeDelete.call(self, Number(index), _array[index]);
+                noticeDelete.call(self, index, _array[index]);
             }
         },
         noticeAllNew = function(){
             var self = this,
                 index;
             for(index in _array){
-                newItem.call(self, Number(index), _array[index]);
+                newItem.call(self, index, _array[index]);
             }
         };
 
-    function ObservableArray(array,callback){
+    function ObservableArray(array, callback){
         _array  = array;
         this.notification = callback;
         noticeAllNew.call(this);
     }
-
-    var i = MAX;
-    while(i--){
-        var b = i;
-        defineProperty(proto,i,{//[i]
-            /*get: (function(i){
-                return function(){//fallback for initial get
-                    //console.log('get',i);
-                    console.log('****************',arguments)
-                    //after new is delete
-                    if(_sign[i]){
-                        //noticeDelete(_array[i]);//delay noticeDelete
+    function ObservableArrayPrototype(){
+        var i               = MAX,
+            defineProperty  = Object.defineProperty,
+            self            = this;
+        /*
+         * =[i]
+         */
+        while(i--){
+            defineProperty(this,i,{//[i]
+                get: (function(i){
+                    return function(){//fallback for initial get
+                        //console.log('get')
+                        return _array[i];
                     }
-                    return _array[i];
-                }
-            })(i),*/
-            set: (function(i){
-                return function(value){//fallback to trigger new
-                    //console.log('set',i,arguments);
-                    //set getter setter for newItem
-                    //new
-                    newItem.call(this, i, value)
-                    _sign[i]    = true;
-                    _array[i]   = value;
-                }
-            })(i)
-        });
+                })(i),
+                set: (function(i){
+                    return function(value){//fallback to trigger new
+                        //console.log('set')
+                        newItem.call(this, i, value);
+                    }
+                })(i)
+            });
+        }
+        /*
+         * =pop
+         */
+        self.pop = function(){
+            var i = _array.length - 1;
+            noticeDelete.call(this, i, _array[i]);
+            return ArrayPrototype.pop.apply(_array,arguments);
+        }
+        /*
+         * =pop
+         */
+        self.pop = function(){
+            var i = _array.length - 1;
+            noticeDelete.call(this, i, _array[i]);
+            return ArrayPrototype.pop.apply(_array,arguments);
+        }
+        /*
+         * =push
+         */
+        self.push = function(){
+            var i   = _array.length;
+            newItem.call(this, i, arguments[0]);
+            return ArrayPrototype.push.apply(_array,arguments);
+        }
+        /*
+         * =reverse
+         */
+        self.reverse = function(){
+            noticeAllDelete.call(this);
+            var result = ArrayPrototype.reverse.apply(_array,arguments);
+            noticeAllNew.call(this);
+            return result;
+        }
+        /*
+         * =shift
+         */
+        self.shift= function(){
+            noticeDelete.call(this, 0, _array[0]);
+            return ArrayPrototype.shift.apply(_array,arguments);
+        }
+        /*
+         * =sort
+         */
+        self.sort= function(){
+            noticeAllDelete.call(this);
+            var deleteItem = ArrayPrototype.sort.apply(_array,arguments);
+            noticeAllNew.call(this);
+            return deleteItem;
+        }
+        /*
+         * =splice
+         */
+        self.splice= function(){
+            var insert  = ArrayPrototype.slice.call(arguments, 2),
+                deleted = ArrayPrototype.splice.apply(_array, arguments),
+                _this   = this,
+                index;
+            for(index in deleted){
+                //notice delete
+                noticeDelete.call(_this, index, deleted[index]);
+            };
+            for(index in insert){
+                //notice add
+                newItem.call(_this, index, insert[index]);
+            };
+            return deleted;
+        }
+        /*
+         * =unshift
+         */
+        self.unshift= function(){
+            var length = ArrayPrototype.unshift.apply(_array, arguments);
+            noticeAllNew.call(this);
+            return length;
+        }
     }
 
-//var endTime   = new Date().getTime();
-    /*
-     * method without notice new,updated,delete
-     */
-    var arr = ['slice','concat','join','toSource','toString','toLocaleString','valueOf'],
-        index,item;
-    for(index in arr){
-        item = arr[index];
-        proto[item] = (function(item){
-            return function(){
-                //console.log('buid-in method',item);
-                return ArrayPrototype[item].apply(_array,arguments);
-            }
-        })(item);
-    };
-    /*
-     * =pop
-     */
-    proto.pop = function(){
-        //console.log('buid-in method','pop');
-        //
-        var i = _array.length - 1;
-        //notice delete
-        noticeDelete.call(this, i, _array[i]);
-        return ArrayPrototype.pop.apply(_array,arguments);
-    }
-    /*
-     * =push
-     */
-    proto.push = function(){
-        //console.log('buid-in method','push');
-        //set getter setter for newItem
-        var i   = _array.length;
-        //notice new
-        newItem.call(this, i, arguments[0]);
-        return ArrayPrototype.push.apply(_array,arguments);
-    }
-    /*
-     * =reverse
-     */
-    proto.reverse = function(){
-        //console.log('buid-in method','reverse');
-        noticeAllDelete.call(this);
-        var result = ArrayPrototype.reverse.apply(_array,arguments);
-        noticeAllNew.call(this);
-        return result;
-    }
-    /*
-     * =shift
-     */
-    proto.shift= function(){
-        //console.log('buid-in method','shift');
-        //notice delete
-        noticeDelete.call(this, 0, _array[0]);
-        return ArrayPrototype.shift.apply(_array,arguments);
-    }
-    /*
-     * =sort
-     */
-    proto.sort= function(){
-        //console.log('buid-in method','sort');
-        noticeAllDelete.call(this);
-        var deleteItem = ArrayPrototype.sort.apply(_array,arguments);
-        noticeAllNew.call(this);
-        //notice delete
-        return deleteItem;
-    }
-    /*
-     * =splice
-     */
-    proto.splice= function(){
-        //console.log('buid-in method','splice');
-        var insert  = Array.prototype.slice.call(arguments, 2),
-            deleted = Array.prototype.splice.apply(_array, arguments),
-            self    = this,
-            index;
-        for(index in deleted){
-            //notice delete
-            noticeDelete.call(self, index, deleted[index]);
-        };
-        for(index in insert){
-            //notice add
-            newItem.call(self, index, insert[index]);
-        };
-        return deleted;
-    }
-    /*
-     * =unshift
-     */
-    proto.unshift= function(){
-        //console.log('buid-in method','unshift');
-        //notice add
-        var length = Array.prototype.unshift.apply(_array, arguments);
-        noticeAllNew.call(this);
-        return length;
-    }
-    /*
-     * test
-     *
-    var a = new ObservableArray([1,2,3],function(change){
-        console.log(change)
-    });
-    a[3]    = 'val';
-    a[3]    = 'update';
-    a.push('push');
-    console.log(a[3],a[4]);
-    console.log(a.pop());
-    a.push('push2');
-    a.reverse();
-    console.log(a.shift());
-    a.push(1);
-    a.push(3);
-    a.push(0);
-    a.sort();
-    a.splice(2,1,'splice');
-    a.unshift('unshift');
-    console.log(a.slice(1))
-    console.log(a.concat([3]))//return a new normal array
-    console.log(a.join(' '))
-    console.log(a.toString())
-    console.log(a.toLocaleString())
-    console.log(a.valueOf())
-    console.log(a)
-    /**/
-    /*
-     * ArrayObserve
-     * ArrayObserve and prop must in the same scope
-     */
+    ObservableArrayPrototype.prototype  = ArrayPrototype;
+    ObservableArray.prototype  = new ObservableArrayPrototype;
+
     function ArrayObserve(obj, prop, callback){
-        //var array   = parent[prop],
-        //    oa      = new ObservableArray(array,callback);
-        //console.log(parent,prop,a)
-        //a = new ObservableArray(a, callback);
-        //console.log(this)
-        //eval("console.log(prop,"+prop+")")
-        //eval(prop+"= new ObservableArray("+prop+",callback)");
-
-        //var caller  = arguments.callee.caller;
-        //set getter setter for parent
-        //delete arr
-        //if(caller == null){//window
-        //}else{//function
-        //}
         obj[prop]   = new ObservableArray(obj[prop],callback);
-        //var arr     = parent[name],
-        //    _arr    = [],
-        //    observeArray    = new ObservableArray(function(change){
-        //    });
-        //Object.defineProperty(parent,name,{
-        //    get: function(){
-        //        return observeArray;
-        //    },
-        //    set: function(){//arr =
-        //        //notice new
-        //    }
-        //})
-        //no need to observe a it self as Object.observe do
     }
-    //window.ObservableArray = ObservableArray;
+
     window.ArrayObserve = ArrayObserve;
-    //console.log(endTime - startTime)
 })()
 
 /*
